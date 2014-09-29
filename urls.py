37988c10323eb39a6c6e03757df605115a73592f
Josh Weinstein <s142028@student.dtu.dk>
2014-09-29 12:21:03 +0000

    Initial Files

---
+++ urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls.defaults import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'Predictive_Beer_Analytics.views.home', name='home'),
+    # url(r'^Predictive_Beer_Analytics/', include('Predictive_Beer_Analytics.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
