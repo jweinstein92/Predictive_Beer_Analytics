d8cc195a8d6baea9a5fa997ed7e17af46b6af44b
jsudket <jimsudket@gmail.com>
2014-10-06 15:15:00 +0200

    Feasability test
    First test to be able to run Django connected to Mysql on
    anywherepython.

--- app/models.py
+++ app/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+
+class Comment(models.Model):
+    text = models.CharField(max_length=60)
--- app/views.py
+++ app/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from app.models import Comment
+from django.shortcuts import render_to_response
+from django.template import Context, RequestContext
+from app.forms import CommentForm
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseNotFound, HttpResponseForbidden
 
-# Create your views here.
+def listEntries(request):
+
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+
+        if form.is_valid():
+            newComment = Comment()
+            newComment.text = request.POST.get('text', '')
+            newComment.save()
+
+    form = CommentForm()
+    commentList = Comment.objects.all()
+    return render_to_response('list.html',{'commentList' : commentList, 'form' : form }, context_instance=RequestContext(request))
+
+def addNewEntry(request):
+
+    return render_to_response('list.html',{}, context_instance=RequestContext(request))
