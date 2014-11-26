bcf1a71c5b4cc04e6b4daed1cbd14ecccc277bb0
jsudket <jimsudket@gmail.com>
2014-11-26 23:27:06 +0100

    Django update
    Added new model for typeData

--- app/models.py
+++ app/models.py
@@ -12,16 +12,24 @@ class Color(models.Model):
     name = models.CharField(max_length=20)
     rating = models.DecimalField(max_digits=6,decimal_places=5)
 
+class Location(models.Model):
+    location = models.CharField(max_length=100)
 
 class BeerStyle(models.Model):
     styleName = models.CharField(max_length=30)
     numRatings = models.BigIntegerField()
 
-class Location(models.Model):
-    location = models.CharField(max_length=100)
 
 class AvbsRange(models.Model):
     range = models.CharField(max_length=100)
+
+class StyleData(models.Model):
+    location = models.ForeignKey(Location)
+    beerStyle = models.ForeignKey(BeerStyle)
+    latcoord = models.TextField(null=True, blank=True)
+    lngcoord = models.TextField(null=True, blank=True)
+    rating = models.TextField(null=True, blank=True)
+
 
 class Abvs(models.Model):
     location = models.ForeignKey(Location)
